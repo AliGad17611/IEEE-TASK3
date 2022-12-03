f6d8273004e5130624e614e6e9bcf1aa0ff93c06
Ali Gad <109631101+AliGad17611@users.noreply.github.com>
2022-12-03 22:46:22 +0200

    Update ps5.py
    update for problem because i forgot /

--- ps5.py
+++ ps5.py
@@ -2,7 +2,7 @@ n,m = map(int,input().split())
 c= sorted(map(int,input().split()))
 m = max(n-c[-1]-1,c[0]-0)
 for i in range(1,len(c)):
-    d = (c[i]-c[i-1])/2
+    d = (c[i]-c[i-1])//2
     if d>m:
         m=d
 print(m)
